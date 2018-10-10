73a9b3cb0c745795e4c2e45d55bcee59c47673e3
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2018-10-10 09:09:19 -0500

    index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 const Pokemon = require('./models/pokemon');
 
-app.get('/', (req, res) => {
-  res.send(Pokemon);
+// app.get('/pokemon', (req, res) => {
+//   res.send(Pokemon);
+// });
+
+
+// app.get('/pokemon/new', (req, res) => {
+//   res.render('index.ejs');
+// });
+app.get('/pokemon', (req, res) => {
+  res.render('index.ejs', {
+    pokemon : Pokemon
+  });
 });
-
-
-
-
 
 
 
