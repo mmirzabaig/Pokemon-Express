c3da02ac45c3db68b1ad0e166be17b6a5af77f90
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2018-10-11 11:47:44 -0500

    finaly working product

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const methodOverride = require('method-override')
 const Pokemon = require('./models/pokemon');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+// app.use('/public', express.static('public'));
 //
 // app.get('/pokemon', (req, res) => {
 //   res.send(Pokemon);
 // });
 
 
-// app.get('/pokemon/new', (req, res) => {
-//   res.render('index.ejs');
-// });
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs');
+});
+app.get('/pokemon/:index/edit', (req, res) => {
+  res.render('edit.ejs', {
+    pokemon : Pokemon[req.params.index],
+    index : req.params.index
+  })
+});
+
+app.post('/pokemon', (req, res) => {
+  Pokemon.push(req.body);
+  res.render('index.ejs', {
+    pokemon : Pokemon
+  });
+})
+
 app.get('/pokemon', (req, res) => {
   res.render('index.ejs', {
     pokemon : Pokemon
@@ -21,6 +43,15 @@ app.get('/pokemon/:index', (req, res) => {
   res.render('show.ejs', {
     pokemon : Pokemon[req.params.index]
   })
+});
+
+
+app.put('/pokemon/:index', (req, res) => {
+  console.log(req.params.id, ' id in the put route');
+  console.log(req.body, ' this should be our form data');
+  Pokemon[req.params.index] = req.body;
+
+  res.redirect('/pokemon');
 });
 
 
